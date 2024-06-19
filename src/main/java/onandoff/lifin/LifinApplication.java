e9fc0319a8623739fc9ef7c219aa08b5cca404c5
JooIcksoo <dlrtn751@gmail.com>
2024-06-19 22:20:00 +0900

    feat(root): initial commit

---
+++ src/main/java/onandoff/lifin/LifinApplication.java
@@ -0,0 +1,13 @@
+package onandoff.lifin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifinApplication.class, args);
+    }
+
+}
